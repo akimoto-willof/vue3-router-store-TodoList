eff9f9a3c3d5909b8bf035cbcf92db6fadb19487
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-11-11 10:30:55 +0900

    first setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// font awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { dom } from "@fortawesome/fontawesome-svg-core";
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+// SB Admin
+import "./assets/vendor/sb-admin-2.min.css";
+
+// アイコンをライブラリに追加して、DOM経由（class）で参照できるようにする。
+library.add(fas);
+dom.watch();
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount("#app");
