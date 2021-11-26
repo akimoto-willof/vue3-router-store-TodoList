b32af1de65551c03a409d011201f1888da7018d4
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-11-26 09:53:20 +0900

    add cleanWebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-const path = require('path')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-        entry: './src/main.js',
-        output: {
-            path: path.resolve(__dirname, './dist'),
-            filename: 'index.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                        },
-                        {
-                            loader: 'css-loader',
-                        }
-                    ]
-                }
-            ]
-        }
-}
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "./vue/main.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./assets/style.css",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
